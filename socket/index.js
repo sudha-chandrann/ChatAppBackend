482a2d3a28eda0c2728b90dbb08409937af6d259
sudhachandrann <sudhachandran1020@gmail.com>
2025-04-13 04:00:39 +0530

    add more cors

--- socket/index.js
+++ socket/index.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173",process.env.FRONTEND_URL],
+    origin: ["http://localhost:5173","http://localhost:5174",process.env.FRONTEND_URL],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH','DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
@@ -40,7 +40,7 @@ app.use("/api/v1/conversations", ConversationRoutes);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173",process.env.FRONTEND_URL],
+    origin: ["http://localhost:5173","http://localhost:5174",process.env.FRONTEND_URL],
     credentials: true,
   },
 });
